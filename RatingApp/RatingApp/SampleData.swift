0f971944f7885c303228c0357dadc65c226f55ba
Amit Kumar <amit.kumar@novanet.net>
2017-11-03 15:51:51 +0530

    Added raw data to display

---
+++ RatingApp/RatingApp/SampleData.swift
@@ -0,0 +1,31 @@
+//
+//  SampleData.swift
+//  RatingApp
+//
+//  Created by Amit on 03/11/17.
+//  Copyright © 2017 Amit. All rights reserved.
+//
+
+import Foundation
+
+final class SampleData {
+    
+    static func generatePlayersData() -> [Player] {
+        return [
+            Player(name: "Bill Evans", game: "Tic-Tac-Toe", rating: 4),
+            Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 5),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 4),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 3),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 5),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 4),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 1),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2),
+            Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 3),
+        ]
+    }
+}
